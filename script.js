e2e7291a73c25dbc059cc8b63d90021a8ccb4b0c
engelberth <engelberth.tech@gmail.com>
2024-05-08 16:57:57 -0700

    Beggining Website files

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var services = [
+        "Regular Cleaning",
+        "Deep Cleaning",
+        "Move-in/Move-out Cleaning",
+        "Special Event Cleaning",
+        "Customized Cleaning Plans"
+    ];
+
+    var servicesList = document.getElementById("servicesList");
+
+    services.forEach(function(service) {
+        var listItem = document.createElement("li");
+        listItem.textContent = service;
+        servicesList.appendChild(listItem);
+    });
+});
